27fec84a5df1b9d91ecd2ea24beeb1d555c55db5
KSlosarczyk <KSlosarczyk@github.com>
2021-04-08 21:49:46 +0100

    #4 Example chart with vue-google

--- src/components/ThirdComponent.vue
+++ src/components/ThirdComponent.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-    TUtaj mozna wrzucic jakis wykres nr 1
+    <GChart
+      type="AreaChart"
+      :data="chartData"
+      :options="chartOptions"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'ThirdComponent',
-}
+  export default {
+    name: 'ThirdComponent',
+    data () {
+      return {
+        // Array will be automatically processed with visualization.arrayToDataTable function
+        chartData: [
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ],
+        chartOptions: {
+            title: 'Company Performance',
+        }
+      }
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import VueGoogleCharts from 'vue-google-charts'
 
-
+Vue.use(VueGoogleCharts)
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
